619efa2ee42419b17f7af1a79249ad4d32cbd8cb
Malgorzata Skrzypecka <malgorzata.skrzypecka@gmail.com>
2018-05-13 13:56:53 +0200

    class file "AnotherClass" added

---
+++ GitTest/AnotherClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest
+{
+    public class AnotherClass
+    {
+        private string _name { get; set; }
+
+        public AnotherClass(string name)
+        {
+            _name = name;
+        }
+    }
+}
