e6a0b65cda271903eb9d4caea3165750912b5259
Malgorzata Skrzypecka <malgorzata.skrzypecka@gmail.com>
2018-05-13 13:54:27 +0200

    change: "Hello Word!" -> "Hello!"

--- GitTest/Program.cs
+++ GitTest/Program.cs
@@ -10,7 +10,7 @@ namespace GitTest
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello!");
             Console.ReadKey();
         }
     }
